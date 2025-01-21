0d6db6f65a6923571b9ff9d80e54b2f909235162
Atul Tiwari <Abc.jnjjatul@gmail.com>
2025-01-21 23:57:15 +0530

    Update changes in terrafrom module

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # create vpc
 module "vpc" {
-  source                  = "../modules/vpc"
+  source                  = "./modules/vpc"
   region                  = var.region
   project_name            = var.project_name
   vpc_cidr                = var.vpc_cidr
@@ -18,7 +18,7 @@ module "vpc" {
 
 # create nat gateway
 module "natgateway" {
-  source                = "../modules/natgateway"
+  source                = "./modules/natgateway"
   public_subnet_az1_id  = module.vpc.public_subnet_az1_id
   internet_gateway      = module.vpc.internet_gateway
   public_subnet_az2_id  = module.vpc.public_subnet_az2_id
@@ -29,14 +29,14 @@ module "natgateway" {
 
 # create security group
 module "security_group" {
-  source = "../modules/security_group"
+  source = "./modules/security_group"
   vpc_id = module.vpc.vpc_id
 
 }
 
 # create alb
 module "application_load_balancer" {
-  source                = "../modules/alb"
+  source                = "./modules/alb"
   project_name          = module.vpc.project_name
   alb_security_group_id = module.security_group.alb_security_group_id
   public_subnet_az1_id  = module.vpc.public_subnet_az1_id
@@ -46,13 +46,13 @@ module "application_load_balancer" {
 
 # create ec2
 module "ec2" {
-  source = "../modules/ec2"
+  source = "./modules/ec2"
   vpc_id = module.vpc.vpc_id
   region = var.region
 }
 #create rds
 module "rds" {
-  source                = "../modules/rds"
+  source                = "./modules/rds"
   vpc_id                = module.vpc.vpc_id
   alb_security_group_id = module.security_group.alb_security_group_id
   secure_subnet_az1_id  = module.vpc.secure_subnet_az1_id
@@ -61,7 +61,7 @@ module "rds" {
 
 # create ASG
 module "asg" {
-  source                    = "../modules/asg"
+  source                    = "./modules/asg"
   project_name              = module.vpc.project_name
   rds_db_endpoint           = module.rds.rds_db_endpoint
   private_subnet_az1_id     = module.vpc.private_subnet_az1_id
